29a48361140f08d1213e826e47ebbc1366541f01
Wae <roahhanchaulagain@gmail.com>
2023-01-11 23:43:29 +0545

    60% login fontend doneadded username and password field along with their icons

--- login.py
+++ login.py
@@ -8,10 +8,11 @@ def loginpage():
     #--------------------Login Window-------------------------------------------------------------
     
     login.title('Welcome to Trella')
-    login.geometry('800x600')
-    login.maxsize(1920,1080)
-    login.resizable()
+    login.geometry('1920x1080')
+    login.resizable(0, 0)
 
+    th_clr='#d6d6d6'
+    fnt='#6c6c6c'
     #--------------------Page Background-------------------------------------------------------------
     img=ImageTk.PhotoImage(Image.open('/home/wae/Documents/giri raj sir/Trella/bg.png'))
     bg=tk.Label(login, image=img)
@@ -29,14 +30,51 @@ def loginpage():
     login.heading=tk.Label(login, 
      text=login.txt,
      font=('yu gothic ui', '28', 'bold'),
-     fg='#6c6c6c').place(x=780, y=450, width=400, height=50)
+     fg='#6c6c6c').place(x=780, y=435, width=400, height=50)
 
 
-    #--------------------"Logo"-------------------------------------------------------------
+    #-----------------------------------"Logo"-------------------------------------------------------------
     logo_img=tk.PhotoImage(file=('/home/wae/Documents/giri raj sir/Trella/llogo.png'))    
     login.logo_lbl=tk.Label(login_frame,image=logo_img)
     login.logo_lbl.image=logo_img
-    login.logo_lbl.place(x=285, y=30)
+    login.logo_lbl.place(x=288, y=25)
+
+
+    #------------------------------"Sign in and Password"---------------------------------------------------
+
+
+    login.username_lbl=tk.Label(login_frame, text='Username', fg=fnt, font=('yu gothic ui', 16, 'bold'))
+    login.username_lbl.place(x=90, y=300)
+    login.username_entry=tk.Entry(login_frame, highlightthickness=5, relief='flat', fg=fnt, bg=th_clr, font=('yu gothic ui', 12, 'bold'))
+    login.username_entry.place(x=88, y=335, width=270)
     
-loginpage() 
+
+    login.passwd_lbl=tk.Label(login_frame, text='Password', fg='#6c6c6c', font=('yu gothic ui', 16, 'bold'))
+    login.passwd_lbl.place(x=90, y=400)
+    login.passwd_entry=tk.Entry(login_frame, highlightthickness=5, relief='flat', fg=fnt, bg=th_clr, font=('yu gothic ui', 12, 'bold'))
+    login.passwd_entry.place(x=88, y=432, width=270)
+
+
+    #--------------------"Sign in and Password icons"-------------------------------------------------------
+   
+   
+    username_img=tk.PhotoImage(file=('/home/wae/Documents/giri raj sir/Trella/user.png'))    
+    login.user_logo_lbl=tk.Label(login_frame,image=username_img)
+    login.user_logo_lbl.image=username_img
+    login.user_logo_lbl.place(x=53, y=339)
+
+
+    passwd_img=tk.PhotoImage(file=('/home/wae/Documents/giri raj sir/Trella/key.png'))    
+    login.passwd_logo_lbl=tk.Label(login_frame,image=passwd_img)
+    login.passwd_logo_lbl.image=passwd_img
+    login.passwd_logo_lbl.place(x=53, y=435)
+
+
+    #--------------------"Password Eye Button"-------------------------------------------------------
+    
+    
+    
+    #--------------------"Login Button"-------------------------------------------------------
+    login.login_btn=ImageTk.PhotoImage(Image.open(''))
+loginpage()
 login.mainloop() 
