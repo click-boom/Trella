0045c897c2c3d2f01b6f170f141cc2beb325f8b8
Wae <roahhanchaulagain@gmail.com>
2023-01-12 20:42:34 +0545

    added icons

--- login1.py
+++ login1.py
@@ -6,7 +6,7 @@ login=tk.Tk()
 
 def loginpage():
     
-    #--------------------Login Window-------------------------------------------------------------
+    #--------------------Login Window------------------------------------------------------------
     
     # screen_width = win.winfo_screenwidth()
     # screen_height = win.winfo_screenheight()
@@ -18,7 +18,7 @@ def loginpage():
 
     #--------------------Functions---------------------------------------------------------------
     
-    th_clr='#d6d6d6'
+    th_clr='#93cbff'
     fnt='#6c6c6c'
     def eye_hidden():
         if  login.passwd_entry.cget("show")=="*":
@@ -30,14 +30,14 @@ def loginpage():
 
 
     #--------------------Page Background-------------------------------------------------------------
-    img=ImageTk.PhotoImage(Image.open('/home/wae/Documents/giri raj sir/Trella/bg.png'))
+    img=ImageTk.PhotoImage(Image.open('/home/wae/Documents/giri raj sir/Trella/rohan.png'))
     bg=tk.Label(login, image=img)
     bg.image=img
     bg.pack(fill='both', expand='yes')
 
 
     #--------------------Login Frame-------------------------------------------------------------
-    login_frame=tk.Frame(login, width='700', height='700')
+    login_frame=tk.Frame(login, width='700', bg=th_clr,height='700')
     login_frame.place(x=620, y=220)
 
 
@@ -46,12 +46,13 @@ def loginpage():
     login.heading=tk.Label(login, 
      text=login.txt,
      font=('yu gothic ui', '30', 'bold'),
-     fg='#6c6c6c').place(x=780, y=435, width=400, height=50)
+     fg='#6c6c6c',
+     bg=th_clr).place(x=780, y=435, width=400, height=50)
 
 
     #-----------------------------------"Logo"-------------------------------------------------------------
-    logo_img=tk.PhotoImage(file=('/home/wae/Documents/giri raj sir/Trella/dlogo.png'))    
-    login.logo_lbl=tk.Label(login_frame,image=logo_img)
+    logo_img=tk.PhotoImage(file=('/home/wae/Documents/giri raj sir/Trella/llogo.png'))    
+    login.logo_lbl=tk.Label(login_frame,image=logo_img, bg=th_clr)
     login.logo_lbl.image=logo_img
     login.logo_lbl.place(x=288, y=25)
 
@@ -59,7 +60,7 @@ def loginpage():
     #------------------------------"Sign in and Password"---------------------------------------------------
 
 
-    login.username_lbl=tk.Label(login_frame, text='Username', fg=fnt, font=('yu gothic ui', 18, 'bold'))
+    login.username_lbl=tk.Label(login_frame, text='Username', fg=fnt,bg=th_clr, font=('yu gothic ui', 18, 'bold'))
     login.username_lbl.place(x=120, y=300)
     login.username_entry=tk.Entry(login_frame, highlightthickness=0, relief='flat', fg=fnt, bg=th_clr, font=('yu gothic ui', 15, 'bold'))
     login.username_entry.place(x=120, y=340, width=450)
@@ -67,7 +68,7 @@ def loginpage():
     login.username_line=tk.Canvas(login_frame, width=400, height=3.0, bg=fnt, highlightthickness=0)
     login.username_line.place(x=120, y=370, width=450)
 
-    login.passwd_lbl=tk.Label(login_frame, text='Password', fg='#6c6c6c', font=('yu gothic ui', 18, 'bold'))
+    login.passwd_lbl=tk.Label(login_frame, text='Password', fg='#6c6c6c', bg=th_clr, font=('yu gothic ui', 18, 'bold'))
     login.passwd_lbl.place(x=120, y=410)
     login.passwd_entry=tk.Entry(login_frame, highlightthickness=0, relief='flat', fg=fnt, bg=th_clr,show="*",font=('yu gothic ui', 15, 'bold'))
     login.passwd_entry.place(x=120, y=450, width=450)
@@ -79,13 +80,13 @@ def loginpage():
    
    
     username_img=tk.PhotoImage(file=('/home/wae/Documents/giri raj sir/Trella/userr-icon.png'))    
-    login.user_logo_lbl=tk.Label(login_frame,image=username_img)
+    login.user_logo_lbl=tk.Label(login_frame,bg=th_clr,image=username_img)
     login.user_logo_lbl.image=username_img
     login.user_logo_lbl.place(x=88, y=342)
 
 
     passwd_img=tk.PhotoImage(file=('/home/wae/Documents/giri raj sir/Trella/pwd_icon.png'))    
-    login.passwd_logo_lbl=tk.Label(login_frame,image=passwd_img)
+    login.passwd_logo_lbl=tk.Label(login_frame,bg=th_clr,image=passwd_img)
     login.passwd_logo_lbl.image=passwd_img
     login.passwd_logo_lbl.place(x=88, y=450)
 
