577b5208afbc8d70b9b43791e3c8467fbb358a84
Haseena <rkc697418@gmail.com>
2023-01-16 11:41:07 +0545

    deleted

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
- # logout_dash=Button(dash_logout_btn,text="Log Out",
-    #                     font=('yu gothic ui',12,'bold'),
-    #                     width=10,
-    #                     bd=0,
-    #                     highlightthickness=0,
-    #                     cursor='hand2',
-    #                     activebackground="#a4a4a4",
-    #                     )
-    # logout_dash.place(x=15,y=600)
