eb210b545f80dc658439402786efe7980b58ece3
sujina <sujinasht307@gmail.com>
2023-01-27 12:50:28 +0545

    edited checklist as dashboard

--- checklist.py
+++ checklist.py
@@ -6,7 +6,7 @@ dash.title('TRELLA')
 dash.geometry('1920x1080')
 dash.minsize(1920, 1080)
 
-icon=tk.PhotoImage(file='/home/wae/Documents/giri raj sir/Trella/Images/llogo.png')
+icon=tk.PhotoImage(file='E:/trella/Images/llogo.png')
 dash .iconphoto(True, icon)
 
 #========================  INDICATOR FUNCTION  ======================================
@@ -36,7 +36,7 @@ def chk_frontend():
     # dashfr.config(bg=lth_clr)
     dashfr.place(x=535, y=300)
     
-    dashfr_img=ImageTk.PhotoImage(Image.open('/home/wae/Documents/giri raj sir/Trella/Images/dash_bg.jpg'))
+    dashfr_img=ImageTk.PhotoImage(Image.open('E:/trella/Images/dash_bg.jpg'))
     dash_bg=tk.Label(dash, image=dashfr_img)
     dash_bg.image=dashfr_img
     dash_bg.pack(fill='both', expand='yes')
@@ -44,8 +44,8 @@ def chk_frontend():
 #------------------  SIDEBAR  ---------------------------
     sidebar =tk.Frame(dash ,bg=dth_clr)    
     sidebar.place(x=30, y=70, width=400, height=800)
-#----------------  Dashboard Heading  -------------------
-    heading =tk.Label(dash , text='Dashboard',font=('yu gothic ui', 44, 'bold'),bg=lth_clr, fg=dfnt)
+#----------------  checklist Heading  -------------------
+    heading =tk.Label(dash , text='Checklist',font=('yu gothic ui', 44, 'bold'),bg=lth_clr, fg=dfnt)
     heading.place(x=950, y=35)
         
 #----------------  Logo Frame  -------------------
@@ -55,20 +55,20 @@ def chk_frontend():
 
     #----------------  Logo Image -------------------
     
-    dash_logo=ImageTk.PhotoImage(Image.open('/home/wae/Documents/giri raj sir/Trella/Images/llogo.png'))
+    dash_logo=ImageTk.PhotoImage(Image.open('E:/trella/Images/llogo.png'))
     logo_lbl=tk.Label(lframe, image=dash_logo, bg= dth_clr)
     logo_lbl.image=dash_logo
     logo_lbl.pack(side='top') 
     
     #----------------  Logo Label -------------------
 
-    logo_lbl=tk.Label(lframe, text='Hello Haseena', bg= dth_clr, font=('yu gothic ui', 25, 'bold'), fg=lfnt)
+    logo_lbl=tk.Label(lframe, text='Hello user', bg= dth_clr, font=('yu gothic ui', 25, 'bold'), fg=lfnt)
     logo_lbl.image=dash_logo
     logo_lbl.pack(side='bottom') 
 
 # ====================  Button Labels  =======================================================================
     #-----------------Checklist--------------------------
-    btn_img=tk.PhotoImage(file='/home/wae/Documents/giri raj sir/Trella/Images/DASH_BTN.png')
+    btn_img=tk.PhotoImage(file='E:/trella/Images/DASH_BTN.png')
     
     
     chk_btn_lbl=tk.Label(sidebar, image=btn_img, bg=dth_clr)
@@ -89,7 +89,7 @@ def chk_frontend():
 
 # ====================  Buttons  ================================================================================
     #-----------------  Checklist  --------------------------
-    chk_img=ImageTk.PhotoImage(Image.open('/home/wae/Documents/giri raj sir/Trella/Images/check-list.png'))
+    chk_img=ImageTk.PhotoImage(Image.open('E:/trella/Images/check-list.png'))
     chk_img.image=chk_img
     global chk_btn
     chk_btn=tk.Button(chk_btn_lbl, text='          CHECKLIST',font=('yu gothic ui', 25, 'bold'), bg=btn_bg, fg=lfnt, cursor='hand2', activebackground=btn_bg,activeforeground='white', bd=0,highlightthickness=0, command=lambda:schk_indicate(chk_btn))
@@ -98,9 +98,9 @@ def chk_frontend():
     chk_img_lbl=tk.Label(chk_btn, image=chk_img, bg=btn_bg)
     chk_img_lbl.place(x=10, y=10)
     
-    #-----------------  reminder  --------------------------
+    #-----------------  dash  --------------------------
     
-    rem_img=ImageTk.PhotoImage(Image.open('/home/wae/Documents/giri raj sir/Trella/Images/reminder.png'))
+    rem_img=ImageTk.PhotoImage(Image.open('E:/trella/Images/dash_bg.jpg'))
     rem_img.image=rem_img
     global rem_btn
     rem_btn=tk.Button(rem_btn_lbl, text='          REMINDER',font=('yu gothic ui', 25, 'bold'), bg=btn_bg, fg=lfnt, cursor='hand2', activebackground=btn_bg, activeforeground='white',bd=0,highlightthickness=0, command=lambda:schk_indicate(rem_btn) )
@@ -111,7 +111,7 @@ def chk_frontend():
 
     #-----------------  sticky notes --------------------------
     
-    st_img=ImageTk.PhotoImage(Image.open('/home/wae/Documents/giri raj sir/Trella/Images/reminder.png'))
+    st_img=ImageTk.PhotoImage(Image.open('E:/trella/Images/dash_bg.jpg'))
     st_img.image=st_img
     global st_btn
     st_btn=tk.Button(st_btn_lbl, text='       STICKY NOTES',font=('yu gothic ui', 25, 'bold'), bg=btn_bg, fg=lfnt, cursor='hand2', activebackground=btn_bg,activeforeground='white' , bd=0,highlightthickness=0, command=lambda:schk_indicate(st_btn) )
@@ -122,15 +122,74 @@ def chk_frontend():
 
 #-----------------  Logout Button  --------------------------
 
-    lcircle_img=tk.PhotoImage(file='/home/wae/Documents/giri raj sir/Trella/Images/L_BTN.png')
+    lcircle_img=tk.PhotoImage(file='E:/trella/Images/L_BTN.png')
     lcircle_img.image=lcircle_img
     lcircle_lbl=tk.Label(sidebar, image=lcircle_img, bg=dth_clr)
     lcircle_lbl.place(x=145, y=660)
 
-    lbtn_img=tk.PhotoImage(file='/home/wae/Documents/giri raj sir/Trella/Images/logout.png')
+    lbtn_img=tk.PhotoImage(file='E:/trella/Images/logout.png')
     lbtn_img.image=lbtn_img
     lbtn=tk.Button(lcircle_lbl, image=lbtn_img, bg=btn_bg, activebackground=btn_bg, cursor='hand2', highlightthickness=0, bd=0)
     lbtn.place(x=19, y=15)
+
+    c1= tk.Checkbutton(dash,
+                       bg="#d6d6d6",
+                       cursor='hand2',
+                       activebackground='#d6d6d6')
+    c1.place(x=450, y=300)
+    
+    c1= tk.Checkbutton(dash,
+                       bg="#d6d6d6",
+                       cursor='hand2',
+                       activebackground='#d6d6d6')
+    c1.place(x=450, y=350)
+
+    c1= tk.Checkbutton(dash,
+                       bg="#d6d6d6",
+                       cursor='hand2',
+                       activebackground='#d6d6d6')
+    c1.place(x=450, y=400)
+    c1= tk.Checkbutton(dash,
+                       bg="#d6d6d6",
+                       cursor='hand2',
+                       activebackground='#d6d6d6')
+    c1.place(x=450, y=450)
+    
+    c1= tk.Checkbutton(dash,
+                       bg="#d6d6d6",
+                       cursor='hand2',
+                       activebackground='#d6d6d6')
+    c1.place(x=450, y=500)
+
+    c1= tk.Checkbutton(dash,
+                       bg="#d6d6d6",
+                       cursor='hand2',
+                       activebackground='#d6d6d6')
+    c1.place(x=450, y=550)
+
+
+    c1= tk.Checkbutton(dash,
+                       bg="#d6d6d6",
+                       cursor='hand2',
+                       activebackground='#d6d6d6')
+    c1.place(x=450, y=600)
+    c1= tk.Checkbutton(dash,
+                       bg="#d6d6d6",
+                       cursor='hand2',
+                       activebackground='#d6d6d6')
+    c1.place(x=450, y=650)
+    
+    c1= tk.Checkbutton(dash,
+                       bg="#d6d6d6",
+                       cursor='hand2',
+                       activebackground='#d6d6d6')
+    c1.place(x=450, y=700)
+
+    c1= tk.Checkbutton(dash,
+                       bg="#d6d6d6",
+                       cursor='hand2',
+                       activebackground='#d6d6d6')
+    c1.place(x=450, y=750)
 
 chk_frontend()
 
