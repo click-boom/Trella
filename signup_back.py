a04fbe4cced3ac7ce5d6090266cbbc354d3bead0
Wae <roahhanchaulagain@gmail.com>
2023-01-30 16:51:36 +0545

    signup back changed insert

--- signup_back.py
+++ signup_back.py
@@ -4,8 +4,8 @@ conn = sqlite3.connect("reg_usrs.db")
 c=conn.cursor()
 # Creating Table
 # c.execute(""" CREATE TABLE users(
-#     username text(10),
-#     email text(30),
+#     username text(10) PRIMARY KEY,
+#     email text(30) UNIQUE , 
 #     epassword text(10),
 #     cpassword text(10)
 # )""")
@@ -13,9 +13,10 @@ c=conn.cursor()
 
 def ins(w, x, y, z):
     with conn:
-        c.execute("INSERT INTO users VALUES (:username,:email, :epassword, :cpassword)",{'username':w, 'email':x,  'epassword':y,  'cpassword':z})
+        c.execute("INSERT INTO users VALUES (?, ?, ?, ?)",[w ,x, y, z])
 
-# ins('', 'rubyrose7@gmail.com', 'rubyruby', 'rubyruby')
+# ins('Rohan', 'roahhanchaulagain@gmail.com', 'rhsn123', 'rhsn123')
+# ins('Ashutosh', 'ashboy@gmail.com', 'bhadre', 'bhadre')
 
 c.execute("DELETE FROM users WHERE email='sandy123@gmail.com'")
 conn.commit()
