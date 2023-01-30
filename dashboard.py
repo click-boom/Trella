1a7f5d1e9e571c1edc3212323f3e193968fdee3b
Wae <roahhanchaulagain@gmail.com>
2023-01-30 18:30:02 +0545

    improved UI

--- dashboard.py
+++ dashboard.py
@@ -23,9 +23,10 @@ def schk_indicate(ind):
 #========================  FRONT END  ======================================
 #-------------------  Theme  -----------------------------
             
-lth_clr='#97C1EC'
-dth_clr='#2A5F7F'
-btn_bg='#5B90B4'
+lth_clr='#A4C7EE'
+dth_clr='#192436'
+btn_bg='#104289'
+frm_clr='#A4C8EB'
 
 lfnt=lth_clr
 dfnt=dth_clr
@@ -35,16 +36,16 @@ def chk_frontend(user_data:dict):
     dashfr=tk.Frame(dash, width=1230, height=746)
     dashfr.place(x=535, y=300)
     
-    dashfr_img=ImageTk.PhotoImage(Image.open('/home/wae/Documents/giri raj sir/Trella/Images/dash_bg.jpg'))
+    dashfr_img=ImageTk.PhotoImage(Image.open('/home/wae/Documents/giri raj sir/Trella/Images/dash_bg1.png'))
     dash_bg=tk.Label(dash, image=dashfr_img)
     dash_bg.image=dashfr_img
     dash_bg.pack(fill='both', expand='yes')
 
 #------------------  SIDEBAR  ---------------------------
     sidebar =tk.Frame(dash ,bg=dth_clr)    
-    sidebar.place(x=30, y=70, width=400, height=800)
+    sidebar.place(x=0, y=0, width=400, height=1100)
 #----------------  Dashboard Heading  -------------------
-    heading =tk.Label(dash , text='Dashboard',font=('yu gothic ui', 44, 'bold'),bg=lth_clr, fg=dfnt)
+    heading =tk.Label(dash , text='Dashboard',font=('yu gothic ui', 44, 'bold'),bg=dth_clr, fg=lfnt)
     heading.place(x=950, y=35)
         
 #----------------  Logo Frame  -------------------
@@ -67,7 +68,7 @@ def chk_frontend(user_data:dict):
 
 # ====================  Button Labels  =======================================================================
     #-----------------Checklist--------------------------
-    btn_img=tk.PhotoImage(file='/home/wae/Documents/giri raj sir/Trella/Images/DASH_BTN.png')
+    btn_img=tk.PhotoImage(file='/home/wae/Documents/giri raj sir/Trella1/Img/dash_btn.png')
     
     
     chk_btn_lbl=tk.Label(sidebar, image=btn_img, bg=dth_clr)
@@ -91,7 +92,7 @@ def chk_frontend(user_data:dict):
     chk_img=ImageTk.PhotoImage(Image.open('/home/wae/Documents/giri raj sir/Trella/Images/check-list.png'))
     chk_img.image=chk_img
     global chk_btn
-    chk_btn=tk.Button(chk_btn_lbl, text='          CHECKLIST',font=('yu gothic ui', 25, 'bold'), bg=btn_bg, fg=lfnt, cursor='hand2', activebackground=btn_bg,activeforeground='white', bd=0,highlightthickness=0, command=lambda:schk_indicate(chk_btn))
+    chk_btn=tk.Button(chk_btn_lbl, text='          CHECKLIST',font=('yu gothic ui', 25, 'bold'), bg=btn_bg, fg=lfnt, cursor='hand2', bd=0,highlightthickness=0)
     chk_btn.place(x=30, y=7)
 
     chk_img_lbl=tk.Label(chk_btn, image=chk_img, bg=btn_bg)
@@ -121,7 +122,7 @@ def chk_frontend(user_data:dict):
 
 #-----------------  Logout Button  --------------------------
 
-    lcircle_img=tk.PhotoImage(file='/home/wae/Documents/giri raj sir/Trella/Images/L_BTN.png')
+    lcircle_img=tk.PhotoImage(file='/home/wae/Documents/giri raj sir/Trella/Images/l_btn.png')
     lcircle_img.image=lcircle_img
     lcircle_lbl=tk.Label(sidebar, image=lcircle_img, bg=dth_clr)
     lcircle_lbl.place(x=145, y=660)
